04e620df03a21f384e0bbce8b9fe53a08d9a6159
Fabio Ruscica <FabioRuscica@hotmail.it>
2023-07-10 17:57:49 +0200

    Removed dummy exports in favour of export categories

--- addons/qodot/src/resources/game-definitions/fgd/qodot_fgd_point_class.gd
+++ addons/qodot/src/resources/game-definitions/fgd/qodot_fgd_point_class.gd
@@ -5,15 +5,13 @@ extends QodotFGDClass
 func _init():
 	prefix = "@PointClass"
 
-@export var scene : String = QodotUtil.CATEGORY_STRING
-
 # The scene file to associate with this PointClass
 # On building the map, this scene will be instanced into the scene tree
+@export_group ("Scene")
 @export var scene_file: PackedScene
-
-@export var scripting: String = QodotUtil.CATEGORY_STRING
 
 # The script file to associate with this PointClass
 # On building the map, this will be attached to any brush entities created
 # via this classname if no scene_file is specified
+@export_group ("Scripting")
 @export var script_class: Script
