95d1eae24f13f92268b8ac058e26d19675b5fde6
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2023-11-04 20:19:09 -0400

    Moved "Apply Rotation" Property to Point Entity Class
    Rotation is a property of all point entities in Trenchbroom, not just those with models. Makes more sense to provide auto rotation control to Point Entity, which Model Point inherits from.

--- addons/qodot/src/resources/game-definitions/fgd/qodot_fgd_model_point_class.gd
+++ addons/qodot/src/resources/game-definitions/fgd/qodot_fgd_model_point_class.gd
@@ -11,8 +11,6 @@ extends QodotFGDPointClass
 ## Model Point Class can override the 'size' meta property by auto-generating a value from the meshes' AABB. Proper generation requires 'scale_expression' set to a float or Vector3.
 ## WARNING: Generated size property unlikely to align cleanly to grid!
 @export var generate_size_property := false
-## Entity will use `angles`, `mangle`, or `angle` to determine rotations on QodotMap build, prioritizing the key value pairs in that order.
-@export var apply_rotation_on_map_build := true
 ## Will auto-generate a .gdignore file in the model export folder to prevent Godot from importing the display models.
 ## Only needs to be generated once and will reset itself on FGD export, so you don't have to track down the right resource if you want to reverse it.
 @export var generate_gd_ignore_file := false
